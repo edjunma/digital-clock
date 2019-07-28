7a3952180319f9e05a7a30a98f3272235d2e058f
edjunma <junwenma@buffalo.edu>
2019-07-28 07:08:21 -0400

    finish building clock

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
-const fullDate = new Date();
-const hours = fullDate.getHours();
-const mins = fullDate.getMinutes();
-const secs = fullDate.getSeconds();
+function clock() {
+	var fullDate = new Date();
+	var hours = fullDate.getHours();
+	var mins = fullDate.getMinutes();
+	var secs = fullDate.getSeconds();
+
+	if (hours < 10) {
+		hours = '0' + hours;
+	}
+	if (mins < 10) {
+		mins = '0' + mins;
+	}
+	if (secs < 10) {
+		secs = '0' + secs;
+	}
+	document.getElementById('hour').innerHTML = hours;
+	document.getElementById('minute').innerHTML = ':' + mins;
+	document.getElementById('second').innerHTML = ':' + secs;
+}
+
+setInterval(clock, 100);
